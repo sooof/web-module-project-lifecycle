cb75973ec0e7770abd66e328f3549074979a3600
sooof <sooof@me.com>
2021-11-30 20:40:02 -0800

    User.js show

--- src/App.js
+++ src/App.js
@@ -2,17 +2,41 @@ import React from 'react';
 import {Link} from 'react-router-dom'
 import User from './components/User';
 import FollowerList from './components/FollowerList';
+import axios from 'axios';
+
 import './App.css';
 
 class App extends React.Component {
+  constructor(){
+    super()
+    this.state = {
+      User: {},
+      Followers:[
+      ]
+    }
+  }
+  componentDidMount() {
+    axios.get('https://api.github.com/users/sooof')
+      .then(resp=> {
+        console.log(resp)
+          this.setState({
+              ...this.state,
+              User: resp.data,
+          });
+      })
+      .catch(err=> {
+          console.log(err);
+      })
+  }
+
   render() {
+    const [follower, setFollower]
+    console.log("App.js user = ", this.state) 
     return(
     <div>
         <nav className="nav-bar">
             <div className="left-links">
                 <Link className="link" to='/'>GITHUN INFO</Link>
-                {/* <Link className="link" to='/products'>Products</Link>  */}
-                
             </div>
             <div className="right-links">
                 {/* <Link className="link" >Search</Link> */}
@@ -26,7 +50,8 @@ class App extends React.Component {
         </nav>
         <div className="App">
           {/* <h1>Github Card</h1> */}
-          <User />
+          <User user={this.state.User}/>
+          <hr/>
           <FollowerList />
         </div>
       
--- src/components/User.js
+++ src/components/User.js
@@ -1,12 +1,24 @@
 import React from "react";
+import './User.css';
 
 
 class User extends React.Component {
 
     render(){
+        console.log("User ", this.props)
         return (
-            <div className="home-div">
-                <h1>User</h1>
+            <div className='user-container '>
+                <div className="user-img">
+                    {/* <img src='https://avatars.githubusercontent.com/u/7962407?v=4'/> */}
+                    <img src={this.props.user.avatar_url} alt="img"/>
+                </div>
+                <div className='user-text'>
+                    <h2>{this.props.user.name}</h2>
+                    <p>({this.props.user.bio})</p>
+                    <h3>TOTAL REPOS: {this.props.user.public_repos}</h3>
+                    <h3>TOTAL FOLLOWERS: {this.props.user.followers}</h3>
+                </div>
+
             </div>
         )
     }
